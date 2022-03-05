53b224d5d5887c58409fa258094e0cf8ab86c3ff
Isabella Beltrame <isabeltrameg12@gmail.com>
2022-03-05 19:01:06 -0300

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+//function hideMenuItems() {
+//  document.getElementById("MenuItems").style.display = "none";
+//}
+
+// var text1 = "
+// 1) Hello Isabella
+// 2) Hello Isabella
+//     ";
+
+// var text2 = '     Hello Isabella';
+
+
+
+// var text3 = `    
+//     <link src
+// `;
+
+// document.head.appendChild('<link src=')
+// document.getElementById().style.position = 'relative'
